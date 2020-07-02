953a014ed787e303f2bbf4305e5ca112293988f1
khurinc <fatkhurr114@gmail.com>
2020-07-03 01:07:27 +0700

    mengganti firestore ke realtime

--- Fire.js
+++ Fire.js
@@ -1,49 +1,24 @@
 import FirebaseKeys from './config';
 import firebase from 'firebase';
 import firestore from 'firebase/firestore';
+// Required for side-effects
+require('firebase/firestore');
 class Fire {
   constructor() {
-    console.log(FirebaseKeys);
     firebase.initializeApp(FirebaseKeys);
   }
-  createdata = async () => {
-    // Add a new document with a generated id.
-    console.log('cliked');
-    firebase
-      .firestore()
-      .collection('cities')
-      .add({
-        name: 'Tokyo',
-        country: 'Japan',
-      })
-      .then(function(docRef) {
-        console.log('Document written with ID: ', docRef.id);
-      })
-      .catch(function(error) {
-        console.error('Error adding document: ', error);
-      });
-  };
   addPost = async ({text, localuri}) => {
     console.log('clicked post');
     const remoteUri = await this.uploadPhotoAsync(localuri);
-    return new Promise((res, rej) => {
-      this.firestore
-        .collection('posts')
-        .add({
-          text,
-          uid: this.uid,
-          timestamp: this.timestamp,
-          image: remoteUri,
-        })
-        .then(ref => {
-          res(ref);
-          console.log('berhasil');
-        })
-        .catch(error => {
-          rej(error);
-          console.log('errornya', error.message);
-        });
-    });
+    this.realdatabase
+      .ref('posts/' + this.uid)
+      .push()
+      .set({
+        text,
+        uid: this.uid,
+        timestamp: this.timestamp,
+        image: remoteUri,
+      });
   };
   uploadPhotoAsync = async uri => {
     const path = 'photos/' + this.uid + '/' + this.timestamp + '.jpg';
@@ -70,10 +45,12 @@ class Fire {
     });
   };
 
-  // get firestore() {
-  //   return firebase.firestore();
-  // }
-
+  get firestore() {
+    return firebase.firestore();
+  }
+  get realdatabase() {
+    return firebase.database();
+  }
   get uid() {
     return (firebase.auth().currentUser || {}).uid;
   }
