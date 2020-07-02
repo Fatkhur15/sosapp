609073895a6426ad8e20ebf1da157f031bb114b0
khurinc <fatkhurr114@gmail.com>
2020-07-03 01:08:22 +0700

    revisi conf

--- config.js
+++ config.js
@@ -13,22 +13,3 @@ const firebaseConfig = {
 };
 
 export default firebaseConfig;
-// import * as firebase from 'firebase';
-// import firestore from 'firebase/firestore';
-
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDFtoR5ykv_0X_XWinxrgY33hI5vOQIXlA',
-//   authDomain: 'sosapp-74ae0.firebaseapp.com',
-//   databaseURL: 'https://sosapp-74ae0.firebaseio.com',
-//   projectId: 'sosapp-74ae0',
-//   storageBucket: 'sosapp-74ae0.appspot.com',
-//   messagingSenderId: '586867121849',
-//   appId: '1:586867121849:web:8208c7b4f547b5fbd5c568',
-//   measurementId: 'G-KEW58DZ23J',
-// };
-
-// firebase.initializeApp(firebaseConfig);
-
-// firebase.firestore();
-
-// export default firebase;
